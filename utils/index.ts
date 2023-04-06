5f2bafc982c515c12138a27bcce98d9ebfaaf8a3
TechAurelian <dev@techaurelian.com>
2023-04-06 10:23:41 +0300

    Export getImageUrl utility function

---
+++ utils/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright (c) TechAurelian {@link https://techaurelian.com}
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Re-export utility functions
+export { getImageUrl } from './utils';
